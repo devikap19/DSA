0007c3152f9c56e5f4dced3b52d12e0eb4143d33
Devika Prabhu <devikaprabhu1911@gmail.com>
2026-07-03 19:52:09 +0530

    Time: 0 ms (100.00%) | Memory: 48.1 MB (70.18%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,48 +1,49 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-      int ans [] = new int[]{-1,-1};
-      int start = 0; 
-      int end = nums.length - 1;
+      int ans[] = new int[]{-1,-1};
+      int low = 0;
+      int high = nums.length - 1;
 
-      while(start <= end)
+      while(low <= high)
       {
-        int mid = start + (end - start) / 2;
-        if(nums[mid] == target)
-        {
-            ans[1] = mid;
-            start = mid + 1;
-        }
-        else if(nums[mid] < target )
-        {
-            start = mid + 1;
-           
-        }
-        else
-        {
-             end = mid -1;
-        }
-      }
-
-      int left = 0, right = nums.length - 1;
-
-      while(left <= right)
-      {
-        int mid = left + (right - left) / 2;
+        int mid = low + (high - low)/2;
         if(nums[mid] == target)
         {
             ans[0] = mid;
-            right = mid - 1;
+            high = mid - 1;
+
+        }
+        else if(nums[mid] > target)
+        {
+            high = mid - 1;
+        }
+        else
+        {
+            low = mid + 1;
+        }
+      }
+
+      int left = 0;
+      int right = nums.length - 1;
+
+      while(left <= right)
+      {
+        int mid = left + (right - left)/2;
+        if(nums[mid] == target)
+        {
+            ans[1] = mid;
+            left = mid + 1;
         }
         else if(nums[mid] < target)
         {
             left = mid + 1;
         }
-        else 
+        else
         {
             right = mid - 1;
         }
       }
-    return ans;
-    }
+      return ans;
 
+    }
 }
