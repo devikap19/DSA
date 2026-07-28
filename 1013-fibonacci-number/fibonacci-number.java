301bab9d6d803fd432e79bd60158d61147bcd0d4
Devika Prabhu <devikaprabhu1911@gmail.com>
2026-07-28 21:57:23 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (33.45%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.java
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -9,16 +9,18 @@ class Solution {
             return 1;
         }
 
+        int secondlast = 0;
+        int last = 1;
+        int curr;
+        for(int i = 2; i <= n; i++)
+        {
+            curr = secondlast + last;
+            secondlast = last;
+            last = curr;
+        }
+
+        return last;
     
-            int fib [] = new int[n + 1];
-            fib[0] = 0;
-            fib[1] = 1;
-            // fib[n] = fib[n - 1] + fib[n - 2];
-            for(int i = 2; i <= n; i++)
-            {
-                fib[i] = fib[i - 1] + fib[i - 2];
-            }
-        
-        return fib[n];
+           
     }
 }
