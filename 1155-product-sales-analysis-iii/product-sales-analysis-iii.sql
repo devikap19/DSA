530bc231c3e641f7678cc1a436a70487515b5090
Devika Prabhu <devikaprabhu1911@gmail.com>
2026-09-20 15:10:09 +0530

    Time: 812 ms (40.57%) | Memory: 0B (100.00%) - LeetSync

--- 1155-product-sales-analysis-iii/product-sales-analysis-iii.sql
+++ 1155-product-sales-analysis-iii/product-sales-analysis-iii.sql
@@ -2,7 +2,7 @@
 SELECT product_id, year AS first_year , quantity, price 
 FROM Sales 
 WHERE (product_id,year) IN (
-    SELECT product_id ,MIN(YEAR)
+    SELECT product_id,MIN(YEAR)
     FROM Sales 
     GROUP BY product_id
 );
